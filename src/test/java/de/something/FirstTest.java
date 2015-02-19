e5399ac3c64862c5d6ce307982ceced2c3495f26
some dude <somedude@company.de>
2015-02-19 22:07:23 +0100

    add some maven project

---
+++ src/test/java/de/something/FirstTest.java
@@ -0,0 +1,14 @@
+package de.something;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstTest {
+
+
+    @Test
+    public void firstTest() throws Exception {
+
+        Assert.assertTrue(true);
+    }
+}
